30eba59d6f015914bb3dd8448008323fa6555238
indraseptama <indraseptama28@gmail.com>
2021-03-30 17:08:20 +0700

    add theme based on theme device

--- App.tsx
+++ App.tsx
@@ -1,8 +1,32 @@
 import React, {Component} from 'react';
 import ViewContainer from './app/bootstrap/ViewContainer';
+import {AppearanceProvider, useColorScheme} from 'react-native-appearance';
+import {
+  DefaultTheme,
+  DarkTheme,
+  NavigationContainer,
+} from '@react-navigation/native';
+const App = () => {
+  const scheme = useColorScheme();
+  console.log(scheme === 'dark');
+  const MyTheme = {
+    dark: false,
+    colors: {
+      primary: 'red',
+      background: 'rgb(242, 242, 242)',
+      card: 'rgb(255, 255, 255)',
+      text: 'rgb(28, 28, 30)',
+      border: 'rgb(199, 199, 204)',
+      notification: 'rgb(255, 69, 58)',
+    },
+  };
+  return (
+    <AppearanceProvider>
+      <NavigationContainer theme={scheme === 'dark' ? MyTheme : DefaultTheme}>
+        <ViewContainer />
+      </NavigationContainer>
+    </AppearanceProvider>
+  );
+};
 
-export default class App extends Component {
-  render() {
-    return <ViewContainer />;
-  }
-}
+export default App;
--- app/modules/tab_a/components/Page1.tsx
+++ app/modules/tab_a/components/Page1.tsx
@@ -1,10 +1,13 @@
+import {useTheme} from '@react-navigation/native';
 import React from 'react';
 import {Button, Text, View} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {NavigationScreenProp} from 'react-navigation';
 
 const Page1 = ({navigation}: {navigation: NavigationScreenProp<any, any>}) => {
+  const {colors} = useTheme();
   return (
-    <View>
+    <View style={{flex: 1, backgroundColor: colors.primary}}>
       <Text>Page 1</Text>
       <Button
         onPress={() => navigation.navigate('Page2')}
